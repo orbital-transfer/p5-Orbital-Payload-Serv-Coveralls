c5c39c1c4a0329bf395a06ca4aa0e9df88d9b90b
Zakariyya Mughal <zaki.mughal@gmail.com>
2022-11-28 19:40:07 -0500

    Remove Modern::Perl

--- lib/Orbital/CLI/Container/Coveralls.pm
+++ lib/Orbital/CLI/Container/Coveralls.pm
@@ -1,8 +1,6 @@
-use Modern::Perl;
+use Orbital::Transfer::Common::Setup;
 package Orbital::CLI::Container::Coveralls;
 # ABSTRACT: Container for Coveralls
-
-use Orbital::Transfer::Common::Setup;
 
 method commands() {
 	return +{
